83f634b5fed954eb63938bae5bccfba2fec249f4
odb13 <oscardb22@gmail.com>
2025-08-20 18:16:37 -0500

    Simple JWT login with two different libraries but using the same principle. and apply a simple SQLAlchemy user table

--- apps/authentication/models/user.py
+++ apps/authentication/models/user.py
@@ -5,7 +5,6 @@ from database import Base, engine
 
 class Users(Base):
     __tablename__ = "users"
-
     id = Column(Integer, primary_key=True, index=True)
     username = Column(String, unique=True)
     email = Column(String, unique=True)
